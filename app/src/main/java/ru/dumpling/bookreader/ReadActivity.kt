6aa3b390ce05b251b8b900c1449f76ca8a7cefda
Виктор Чурилов <vchurilov@yamoney.ru>
2018-09-23 21:12:49 +0300

    first run

--- app/src/main/java/ru/dumpling/bookreader/ReadActivity.kt
+++ app/src/main/java/ru/dumpling/bookreader/ReadActivity.kt
@@ -7,6 +7,7 @@ class ReadActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.activity_read)
     }
+
 }
