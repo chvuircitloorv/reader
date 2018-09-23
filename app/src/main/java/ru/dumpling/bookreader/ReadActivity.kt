2ae953baaa7a435178a42aa35516699020094667
Виктор Чурилов <vchurilov@yamoney.ru>
2018-09-23 20:59:04 +0300

    Initial commit

---
+++ app/src/main/java/ru/dumpling/bookreader/ReadActivity.kt
@@ -0,0 +1,12 @@
+package ru.dumpling.bookreader
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ReadActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
